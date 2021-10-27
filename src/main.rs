de1ff4cf51b3e2386e26f9456595aa1d67d5f8a7
mazi <602281666@qq.com>
2021-10-27 11:17:54 +0800

    单元测试

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@
 #![no_main] // disable all Rust-level entry points
 // #![feature(asm)]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod serial;
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -29,12 +35,61 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     // panic!("Some panic message");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-/// This function is called on panic.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    // println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+// #[test_case]
+// fn trivial_assertion() {
+//     // print!("trivial assertion... ");
+//     serial_print!("trivial assertion... ");
+//     assert_eq!(0, 1);
+//     // println!("[ok]");
+//     serial_println!("[ok]");
+// }
